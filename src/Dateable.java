5909c0dd482fd5e3989daec45200afbebdb5ffa9
zhongmouhuang <zhongmouh2027@student.cis.edu.hk>
2024-08-26 08:52:28 +0800

    finished first lab

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("Enter your age: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int age = scanner.nextInt();
+        int ageToDate = 7 + (age/2);
+        System.out.println("Your optimal age to date is: " + ageToDate);
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        System.out.println("Enter your age: ");
 
+        Scanner scanner = new Scanner(System.in);
+
+
+        int age = scanner.nextInt();
+        int pageToRead = (100 - age);
+        System.out.println("Your optimal age to date is: " + pageToRead);
     }
 }
